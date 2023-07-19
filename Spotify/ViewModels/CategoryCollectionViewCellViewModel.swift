31d642bf3dc6f5f891d9cd49ab2103761b110148
Dima <dorogovd@yandex.ru>
2023-07-19 23:50:55 +0300

    Search Categories UI and fetch
    - Response Model
    - Category Model
    - Icons
    - CategoryCollectionViewCellViewModel

---
+++ Spotify/ViewModels/CategoryCollectionViewCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryCollectionViewCellViewModel.swift
+//  Spotify
+//
+//  Created by Dmitrii Dorogov on 19.07.2023.
+//
+
+import Foundation
+
+struct CategoryCollectionViewCellViewModel {
+    let title: String
+    let artworkURL: URL?
+}
