45193bf5d7a2703e3614c87622733257fe33a7ae
Dima <dorogovd@yandex.ru>
2023-08-02 16:51:00 +0300

    Custom search table view cells
    - SearchResultCells (Views class):
      - SearchResultDefaultTableViewCell
    - SearchResultSubtitleTableViewCell (Views class)
    - SearchResultDefaultTableViewCellViewModel (ViewModels)
    - SearchResultSubtitleTableViewCellViewModel (ViewModels)

---
+++ Spotify/ViewModels/SearchResultDefaultTableViewCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResultDefaultTableViewCellViewModel.swift
+//  Spotify
+//
+//  Created by Dmitrii Dorogov on 01.08.2023.
+//
+
+import Foundation
+
+struct SearchResultDefaultTableViewCellViewModel {
+    let title: String
+    let imageURL: URL?
+}
---
+++ Spotify/ViewModels/SearchResultSubtitleTableViewCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResultSubtitleTableViewCellViewModel.swift
+//  Spotify
+//
+//  Created by Dmitrii Dorogov on 02.08.2023.
+//
+
+import Foundation
+
+struct SearchResultSubtitleTableViewCellViewModel {
+    let title: String
+    let subtitle: String
+    let imageURL: URL?
+}
