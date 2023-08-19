ffc5c70687cf6337553bba3c787c09e87bf1f158
Dima <dorogovd@yandex.ru>
2023-08-19 16:47:17 +0300

    UI for the Library tab
    - LibraryToggleView
    - LibraryPlaylistsViewController
    - LibraryAlbumsViewController

---
+++ Spotify/Controllers/Other/Library/LibraryAlbumsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryAlbumsViewController.swift
+//  Spotify
+//
+//  Created by Dmitrii Dorogov on 11.08.2023.
+//
+
+import UIKit
+
+class LibraryAlbumsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemOrange
+    }
+
+}
---
+++ Spotify/Controllers/Other/Library/LibraryPlaylistsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryPlaylistsViewController.swift
+//  Spotify
+//
+//  Created by Dmitrii Dorogov on 11.08.2023.
+//
+
+import UIKit
+
+class LibraryPlaylistsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+
+}
